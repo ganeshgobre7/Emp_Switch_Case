09c8f63d3ca3b14bc4473c386d8e7c20830c7cd6
ganeshgobre7 <ganeshgobre7@gmail.com>
2021-09-01 17:34:53 +0530

    Use case 2 FullTime Employee Wage

--- Emp_Wages/Emp_Wages/Program.cs
+++ Emp_Wages/Emp_Wages/Program.cs
@@ -10,10 +10,16 @@ namespace Emp_Wages
             Random random = new Random();
             int empCheck = random.Next(0, 2);
 
+            int Emp_Full_Time = 8;
+            int Per_Hour_Sal = 20;
+            int Total_Sal = 0;
+
             switch (empCheck)
             {
                 case 1:
-                    Console.WriteLine("Employee is Present");
+                    Console.WriteLine("Employee is FullTime");
+                    Total_Sal = Emp_Full_Time * Per_Hour_Sal;
+                    Console.WriteLine("Employee Total Salary is:" + Total_Sal);
                     break;
 
                 default:
