d593c35c1432847afdb8d3017e59f0aea54c898c
ganeshgobre7 <ganeshgobre7@gmail.com>
2021-09-01 17:41:59 +0530

    Use Case 3 Employee PartTime Wage

--- Emp_Wages/Emp_Wages/Program.cs
+++ Emp_Wages/Emp_Wages/Program.cs
@@ -8,9 +8,10 @@ namespace Emp_Wages
         {
             Console.WriteLine("**Welcome To Employee Wage Computation**");
             Random random = new Random();
-            int empCheck = random.Next(0, 2);
+            int empCheck = random.Next(0, 3);
 
             int Emp_Full_Time = 8;
+            int Emp_Part_Time = 4;  
             int Per_Hour_Sal = 20;
             int Total_Sal = 0;
 
@@ -19,6 +20,12 @@ namespace Emp_Wages
                 case 1:
                     Console.WriteLine("Employee is FullTime");
                     Total_Sal = Emp_Full_Time * Per_Hour_Sal;
+                    Console.WriteLine("Employee Total Salary is:" + Total_Sal);
+                    break;
+
+                case 2:
+                    Console.WriteLine("Employee is PartTime");
+                    Total_Sal = Emp_Part_Time * Per_Hour_Sal;
                     Console.WriteLine("Employee Total Salary is:" + Total_Sal);
                     break;
 
