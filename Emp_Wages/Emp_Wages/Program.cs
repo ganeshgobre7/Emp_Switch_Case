d4b7efa97021edcf3b463d4ed43cf46152371c30
ganeshgobre7 <ganeshgobre7@gmail.com>
2021-09-01 19:47:56 +0530

    Use Case 5 Calculate Wages for a months

--- Emp_Wages/Emp_Wages/Program.cs
+++ Emp_Wages/Emp_Wages/Program.cs
@@ -13,23 +13,27 @@ namespace Emp_Wages
             int Emp_Full_Time = 8;
             int Emp_Part_Time = 4;  
             int Per_Hour_Sal = 20;
+            int One_Day_Sal = 0;
+            int Num_Work_Day = 20;
             int Total_Sal = 0;
-            int Num_Work_Day = 20; ;
-
-            for(int day=0; day < Num_Work_Day; day++)
 
             switch (empCheck)
             {
                 case 1:
                     Console.WriteLine("Employee is FullTime");
-                    Total_Sal = Emp_Full_Time * Per_Hour_Sal;
-                    Console.WriteLine("Employee Total Salary is:" + Total_Sal);
+                    One_Day_Sal = Emp_Full_Time * Per_Hour_Sal;
+                    Console.WriteLine("Employee Total Salary is:" + One_Day_Sal);
+                    Total_Sal = One_Day_Sal * Num_Work_Day;
+                    Console.WriteLine("Total Salary of Months:" + Total_Sal);
                     break;
 
                 case 2:
                     Console.WriteLine("Employee is PartTime");
-                    Total_Sal = Emp_Part_Time * Per_Hour_Sal;
-                    Console.WriteLine("Employee Total Salary is:" + Total_Sal);
+                    One_Day_Sal = Emp_Part_Time * Per_Hour_Sal;
+                    Console.WriteLine("Employee Total Salary is:" + One_Day_Sal);
+                        Total_Sal = One_Day_Sal * Num_Work_Day;
+                        Console.WriteLine("Total Salary of Months:" + Total_Sal);
+
                     break;
 
                 default:
