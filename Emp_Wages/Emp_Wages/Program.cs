bbfcf8e6a7609e8b9d3357566d0261acf680b755
ganeshgobre7 <ganeshgobre7@gmail.com>
2021-09-01 17:19:35 +0530

    Use case 1 Employee Present Success

---
+++ Emp_Wages/Emp_Wages/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Emp_Wages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**Welcome To Employee Wage Computation**");
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+
+            switch (empCheck)
+            {
+                case 1:
+                    Console.WriteLine("Employee is Present");
+                    break;
+
+                default:
+                    Console.WriteLine("Employee is Absent");
+                    break;
+            }
+        }
+    }
+}
